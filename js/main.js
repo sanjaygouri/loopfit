2a192fb8c636e432376b96df3acec8b1c1313ce4
sanjay <sanjaygouri77@gmail.com>
2016-09-07 20:38:23 +0530

    initial commit
    initial commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var tl = new TimelineLite();
+tl.to(".b2c", 6,{x:"78%"}, delay=0.5);
+tl.to(".loopfitlogo",0.1,{y:-100,autoAlpha:0,ease:Back.easeIn});
+tl.to(".b2c",0.1,{y:-100,autoAlpha:0,ease:Back.easeIn});
